f25a9db87ee04b8b65fac1a8a1f30b7e6c3e21dd
Igor Kislyuk <igorkislyuk@icloud.com>
2017-02-24 02:23:30 +0300

    Add. Circle colors

--- IKProgressView/Views/IKProgressView.swift
+++ IKProgressView/Views/IKProgressView.swift
@@ -4,146 +4,223 @@
 //
 
 import UIKit
+import QuartzCore
 
-class IKProgressView: UIView {
+// todo: README
+// todo: license
+// todo: travis
+// todo: IBDesignable
+// todo: reposition
+
+class IKProgressView: UIView, CAAnimationDelegate {
 
     private var progressLabel: UILabel
     private var progressLayer = CAShapeLayer()
+
+    private var gradient: CAGradientLayer?
+    private var colors = [CGColor]()
     
+    private var layers = [CAShapeLayer]()
+    private var finished = false
+
     override class var layerClass: AnyClass {
         get {
             return CAShapeLayer.self
         }
     }
-    
-    
+
+
     required init?(coder aDecoder: NSCoder) {
         progressLabel = UILabel()
         super.init(coder: aDecoder)
-        
+
         commonInit()
     }
-    
+
     override init(frame: CGRect) {
         progressLabel = UILabel()
         super.init(frame: frame)
-        
+
         commonInit()
     }
-    
+
     private func commonInit() {
-        createProgressLayer()
-        
         self.backgroundColor = UIColor.clear
-        
+
+//        testColors()
+        createProgressLayer()
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         //todo: add functionality
     }
-    
-    // TODO: extract values for IB
-    
+
     func createLabel() {
-        
-        progressLabel = UILabel(frame: CGRect(x:0.0, y:0.0, width:frame.width, height:60.0))
+
+        progressLabel = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: frame.width, height: 60.0))
         progressLabel.textColor = .white
         progressLabel.textAlignment = .center
         progressLabel.text = "Load content"
         progressLabel.font = UIFont(name: "HelveticaNeue-UltraLight", size: 40.0)
         progressLabel.translatesAutoresizingMaskIntoConstraints = false
         addSubview(progressLabel)
-        
+
         addConstraint(NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: progressLabel, attribute: .centerX, multiplier: 1.0, constant: 0.0))
         addConstraint(NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: progressLabel, attribute: .centerY, multiplier: 1.0, constant: 0.0))
     }
     
+    func createShape(with path: CGPath, from: (start: Float, end: Float), color: CGColor) -> CAShapeLayer {
+    
+        let layer = CAShapeLayer()
+        
+        layer.path = path
+        layer.strokeColor = color
+        layer.backgroundColor = nil
+        layer.fillColor = nil
+        layer.lineWidth = 30.0
+        
+        layer.strokeStart = from.start.cgFloat
+        layer.strokeEnd = from.end.cgFloat
+        
+        return layer
+    }
+
     func createProgressLayer() {
         
-        //create circle
-        let layer = CAShapeLayer()
+        colors = rotateColors(colors)
         
         let startAngle: CGFloat = 0
         let endAngle: CGFloat = CGFloat(M_PI * 2)
         let center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
-        let path = UIBezierPath(arcCenter: center, radius: 150 /*size of circle width*/, startAngle: startAngle, endAngle: endAngle, clockwise: false).cgPath
+        let path = UIBezierPath(arcCenter: center, radius: 150, startAngle: startAngle, endAngle: endAngle, clockwise: false).cgPath
         
-        layer.path = path
-        layer.strokeColor = UIColor.red.cgColor
-        layer.backgroundColor = nil
-        layer.fillColor = nil
-        layer.lineWidth = 30.0
-//        layer.lineCap = kCALineCapRound
+        let size = 0.006.float
+        
+        for (index, color) in colors.enumerated() {
+            
+            let start = index.float.multiplied(by: size)
+            let end = (index + 1).float.multiplied(by: size)
+            
+            let shape = createShape(with: path, from: (start, end), color: color)
+            
+            layer.addSublayer(shape)
+            
+            layers.append(shape)
+        }
+        
+        performAnimation()
 
-        layer.strokeStart = 0.001
-        layer.strokeEnd = 0.002
+    }
+
+    func testColors() {
+
+        colors = rotateColors(colors)
+
+        let gradient = CAGradientLayer()
+        gradient.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
+        gradient.colors = colors
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+
+        layer.addSublayer(gradient)
         
-        let layer2 = CAShapeLayer()
+        self.gradient = gradient
         
-//        let startAngle: CGFloat = 0
-//        let endAngle: CGFloat = CGFloat(M_PI * 2)
-//        let center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
-//        let path = UIBezierPath(arcCenter: center, radius: 150 /*size of circle width*/, startAngle: startAngle, endAngle: endAngle, clockwise: false).cgPath
+        performAnimation()
+
+    }
+    
+    func performAnimation() {
         
-        layer2.path = path
-        layer2.strokeColor = UIColor.blue.cgColor
-        layer2.backgroundColor = nil
-        layer2.fillColor = nil
-        layer2.lineWidth = 30.0
-        //        layer.lineCap = kCALineCapRound
+        let oldColors = colors
+        let newColors = rotateColors(colors)
         
-        layer2.strokeStart = 0.002
-        layer2.strokeEnd = 0.003
-//        layer.mask = maskLayer()
+        for (index, layer) in layers.enumerated() {
+            
+            layer.strokeColor = newColors[index]
+            
+            let animation = CABasicAnimation(keyPath: "strokeColor")
+            animation.fromValue = oldColors[index]
+            animation.toValue = newColors[index]
+            
+            
+            animation.duration = 0.08
+            animation.isRemovedOnCompletion = true
+            animation.fillMode = kCAFillModeForwards
+            animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+            
+            if (index == layers.endIndex) {
+                animation.delegate = self
+            }
+            
+            layer.add(animation, forKey: nil)
+        }
         
-//        let layer = CAGradientLayer()
-//        layer.frame = bounds
-//        layer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
-//        
-//        let locations: [NSNumber] = [0.4, 0.8]
-//        layer.locations = locations
-        
-        
-        self.layer.addSublayer(layer)
-        self.layer.addSublayer(layer2)
-        
-//        progressLayer = layer
-        
-//        let animation = CABasicAnimation(keyPath: "strokeColor")
-//        animation.fromValue = UIColor.red.cgColor
-//        animation.toValue = UIColor.blue.cgColor
-//        
-//        let animation2 = CABasicAnimation(keyPath: "strokeEnd")
-//        animation2.fromValue = NSNumber(value: 0.6)
-//        animation2.toValue = NSNumber(value: 1.0)
-//        
-//        let animation3 = CABasicAnimation(keyPath: "strokeStart")
-//        animation3.fromValue = NSNumber(value: 0.0)
-//        animation3.toValue = NSNumber(value: 0.6)
-//        
-//        let group = CAAnimationGroup()
-//        group.animations = [animation, animation2, animation3]
-//        group.duration = 2.0
-//        
-//        layer.add(group, forKey: nil)
+        colors = newColors
         
     }
     
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        performAnimation()
+    }
+
+    private func rotateColors(_ colors: [CGColor]) -> [CGColor] {
+
+        var colors = colors
+
+        guard colors.count == 0 else {
+            return Array(colors.suffix(colors.count - 1) + colors.prefix(1))
+        }
+
+        for i in 0 ... 72 {
+
+            let hueValue: CGFloat = (i * 5).float.divided(by: 360).cgFloat
+            let color = UIColor(hue: hueValue, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+
+            colors.append(color.cgColor)
+        }
+
+        return colors
+    }
+
     private func maskLayer() -> CAShapeLayer {
         let layer = CAShapeLayer()
-        
-//        let startAngle: CGFloat = 0
-//        let endAngle: CGFloat = CGFloat(M_PI * 2)
+
         let center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
         let rect = CGRect(origin: center, size: CGSize(width: 100, height: 140))
         let path = UIBezierPath(rect: rect).cgPath
-        
+
         layer.path = path
-        
+
         return layer
     }
-    
 
 
+}
+
+extension Double {
+    var float: Float {
+        return Float(self)
+    }
+}
+
+extension Int {
+    var float: Float {
+        return Float(self)
+    }
+}
+
+extension Float {
+    var cgFloat: CGFloat {
+        return CGFloat(self)
+    }
+}
+
+extension UIColor {
+    var hsba: (h: CGFloat, s: CGFloat, b: CGFloat, a: CGFloat) {
+        var hsba: (h: CGFloat, s: CGFloat, b: CGFloat, a: CGFloat) = (0, 0, 0, 0)
+        self.getHue(&(hsba.h), saturation: &(hsba.s), brightness: &(hsba.b), alpha: &(hsba.a))
+        return hsba
+    }
 }
