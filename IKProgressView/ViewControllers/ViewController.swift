098b7d750ae9cf8ebe596a25975f0f23d16eed2f
Igor Kislyuk <igorkislyuk@icloud.com>
2017-02-26 14:37:17 +0300

    Bug. Progress doesn't set with animation

--- IKProgressView/ViewControllers/ViewController.swift
+++ IKProgressView/ViewControllers/ViewController.swift
@@ -10,14 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressView: IKProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        let view = IKProgressView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
-//        self.view.addSubview(view)
+        let delta = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + delta) {
+            self.progressView.progress = 50
+            DispatchQueue.main.asyncAfter(deadline: .now() + delta, execute: {
+                self.progressView.progress = 73
+                DispatchQueue.main.asyncAfter(deadline: .now() + delta, execute: {
+                    self.progressView.progress = 100
+                })
+            })
+        }
     }
 
+    @IBAction func actionValueChanged(_ sender: Any) {
+        if let sw = sender as? UISwitch {
+            progressView.animated = sw.isOn
+        }
+    }
+    
+    @IBAction func actionFilledChanged(_ sender: Any) {
+        if let sw = sender as? UISwitch {
+            progressView.filledView = sw.isOn
+        }
+    }
+    
+    @IBAction func actionOuterRChanged(_ sender: Any) {
+        if let sl = sender as? UISlider {
+            progressView.exteriorR = Int(sl.value)
+        }
+    }
+    
+    @IBAction func actionInnerRChanged(_ sender: Any) {
+        if let sl = sender as? UISlider {
+            progressView.interiorR = Int(sl.value)
+        }
+    }
+    
+    @IBAction func actionElementsChanged(_ sender: Any) {
+        if let sl = sender as? UISlider {
+            print("Elements: " + String(sl.value))
+            progressView.elements = Int(sl.value)
+        }
+    }
+    
+    @IBAction func actionProgressChanged(_ sender: Any) {
+        if let sl = sender as? UISlider {
+            print("Progress: " + String(sl.value))
+            progressView.progress = Int(sl.value)
+        }
+    }
+    
+    
 
 }
 
--- IKProgressView/Views/IKProgressView.swift
+++ IKProgressView/Views/IKProgressView.swift
@@ -6,9 +6,6 @@
 import UIKit
 import QuartzCore
 
-//todo: deal with setting from IB
-//todo: deal with background from view
-
 //todo: make pod
 //todo: make carthage
 //todo: generate doc
@@ -23,12 +20,8 @@ class IKProgressView: UIView, CAAnimationDelegate {
         }
     }
     
-    /// Is gradient will spinning
-    @IBInspectable var animated: Bool = true {
-        didSet {
-            setNeedsDisplay()
-        }
-    }
+    /// Count of trapezoids
+    @IBInspectable var elements: Int = 512
     
     /// Will use view height and width
     @IBInspectable var filledView: Bool = true {
@@ -51,19 +44,30 @@ class IKProgressView: UIView, CAAnimationDelegate {
         }
     }
     
+    /// Will gradient spinning
+    public var animated: Bool = true {
+        didSet {
+            _timer.isPaused = !animated
+        }
+    }
     /// Times progress view will draw itself
-    public var fps: Int = 120
+    public var fps: Int = 240
     /// For neat visual experience with border
     public var scale: CGFloat = 0.99
+    
+    /// Set progress animated
+    ///
+    /// - Parameters:
+    ///   - progress: new progress
+    ///   - animated: animated flag
+    public func setProgress(_ progress: Int, animated: Bool) {
+        
+    }
 
     // MARK: - private section
     
     private var _colors = [UIColor]()
-    private var _elementsCount: Int {
-        return progress * 5 + 12
-    }
-
-    private var _timer: Timer?
+    private var _timer: CADisplayLink!
     
     private var _progress: CGFloat {
         return CGFloat(progress) / CGFloat(100)
@@ -72,20 +76,19 @@ class IKProgressView: UIView, CAAnimationDelegate {
 
     required override init(frame: CGRect) {
         super.init(frame: frame)
-        setupTimer()
+        enableAnimation()
     }
 
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
         
-        self.animated = aDecoder.decodeBool(forKey: "animated")
         self.progress = aDecoder.decodeInteger(forKey: "progress")
         self.fps = aDecoder.decodeInteger(forKey: "fps")
         self.filledView = aDecoder.decodeBool(forKey: "filledView")
         self.interiorR = aDecoder.decodeInteger(forKey: "interiorR")
         self.exteriorR = aDecoder.decodeInteger(forKey: "exteriorR")
         
-        setupTimer()
+        super.init(coder: aDecoder)
+        enableAnimation()
     }
 
     override func layoutSubviews() {
@@ -94,28 +97,28 @@ class IKProgressView: UIView, CAAnimationDelegate {
         setNeedsDisplay()
     }
 
-    func setupTimer() {
-
-        _timer?.invalidate()
+    func enableAnimation() {
         
-        if !animated {
-            return
+        _timer = CADisplayLink(target: self, selector: #selector(self.animate))
+        _timer.add(to: RunLoop.current, forMode: .defaultRunLoopMode)
+        
+        if #available(iOS 10, *) {
+            _timer.preferredFramesPerSecond = fps
+        } else {
+            _timer.frameInterval = 60 / fps
         }
-
-        _timer = Timer.scheduledTimer(withTimeInterval: 1.0 / Double(fps), repeats: true, block: {
-            [weak self]
-            (timer) in
-
-            self?.setNeedsDisplay()
-
-        })
+        
+    }
+    
+    @objc private func animate() {
+        self.setNeedsDisplay()
     }
 
     override func draw(_ rect: CGRect) {
-
+        
         _colors = rotateColors(_colors)
 
-        UIColor.white.set()
+        backgroundColor?.set()
         UIRectFill(bounds)
 
         var r, R: Int
@@ -131,8 +134,8 @@ class IKProgressView: UIView, CAAnimationDelegate {
         let halfInteriorPerim = M_PI * Double(r)
         let halfExteriorPerim = M_PI * Double(R)
 
-        let smallBase = halfInteriorPerim.divided(by: Double(_elementsCount))
-        let largeBase = halfExteriorPerim.divided(by: Double(_elementsCount))
+        let smallBase = halfInteriorPerim.divided(by: Double(elements))
+        let largeBase = halfExteriorPerim.divided(by: Double(elements))
 
         let trapezoid = UIBezierPath()
         trapezoid.move(to: CGPoint(x: -smallBase.divided(by: 2.0), y: Double(r)))
@@ -143,14 +146,14 @@ class IKProgressView: UIView, CAAnimationDelegate {
 
         trapezoid.close()
 
-        var increment: CGFloat = CGFloat(2.0 * M_PI) / CGFloat(_elementsCount)
-        increment = increment * _progress
+        let increment: CGFloat = CGFloat(2.0 * M_PI) * _progress / CGFloat(elements)
         if let context = UIGraphicsGetCurrentContext() {
 
             context.translateBy(x: bounds.width.divided(by: 2.0), y: bounds.height.divided(by: 2))
 
             context.scaleBy(x: scale, y: scale)
-            context.rotate(by: CGFloat(M_PI_2) * CGFloat(3.0))
+            context.rotate(by: CGFloat(M_PI_2) * CGFloat(3.0)) //to start
+            context.rotate(by: -increment / 2)
 
             for color in _colors {
 
@@ -173,9 +176,9 @@ class IKProgressView: UIView, CAAnimationDelegate {
             return Array(colors.suffix(1) + colors.prefix(colors.count - 1))
         }
 
-        for i in 0 ..< _elementsCount {
+        for i in 0 ..< elements {
 
-            let hueValue: CGFloat = CGFloat(i) / CGFloat(_elementsCount)
+            let hueValue: CGFloat = CGFloat(i) / CGFloat(elements)
             let color = UIColor(hue: hueValue, saturation: 1.0, brightness: 1.0, alpha: 1.0)
 
             colors.append(color)
@@ -186,10 +189,3 @@ class IKProgressView: UIView, CAAnimationDelegate {
 
 }
 
-extension UIColor {
-    var hsba: (h: CGFloat, s: CGFloat, b: CGFloat, a: CGFloat) {
-        var hsba: (h: CGFloat, s: CGFloat, b: CGFloat, a: CGFloat) = (0, 0, 0, 0)
-        self.getHue(&(hsba.h), saturation: &(hsba.s), brightness: &(hsba.b), alpha: &(hsba.a))
-        return hsba
-    }
-}
